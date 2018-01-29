ad516750cb7b11ba60fa44e4c95f022f133c136e
Thomas LACAZE <contact@thomaslacaze.fr>
2018-01-29 20:22:02 +0100

    Add commentaries

--- javaCompile.sh
+++ javaCompile.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-FILE=$1
+# file = filename + extension
+file=$1
 filename=${FILE%%.*}
 extension=${FILE##*.}
-if [ $# -eq 1 ]; then
-    if [ -f $FILE ]; then
-        if [[ "$extension" == "java" ]]; then
+if [ $# -eq 1 ]; then # Test if the number of parameters is to 1
+    if [ -f $file ]; then # Test if the file is really a file
+        if [[ "$extension" == "java" ]]; then # And test if the extension is "java" and not "class"
             rm *.class
-            javac $FILE
+            javac $file
             java $filename
         else
             echo "Error don't forget to use the .java not .class"
